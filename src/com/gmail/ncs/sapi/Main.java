99c6c6e8d62cde0008c2c5bf32f46baf748edaba
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-04 23:56:06 +0000

    Exercise to practise how to use Java Classes. Build a simple calculator with the 4 simple operation (+, -, *, /)

---
+++ src/com/gmail/ncs/sapi/Main.java
@@ -0,0 +1,10 @@
+package com.gmail.ncs.sapi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    SimpleCalculator first = new SimpleCalculator(4, 7);
+        System.out.println(first.number1 + first.number2);
+    }
+}
---
+++ src/com/gmail/ncs/sapi/SimpleCalculator.java
@@ -0,0 +1,12 @@
+package com.gmail.ncs.sapi;
+
+public class SimpleCalculator {
+
+    int number1;
+    int number2;
+
+    public SimpleCalculator(int a, int b){
+        number1 = a;
+        number2 = b;
+    }
+}
