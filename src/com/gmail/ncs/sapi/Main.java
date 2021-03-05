24fa804251b59a70b3c484d621ca1d871fbc9ccf
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-05 14:44:07 +0000

    add Scanner to get input values

--- src/com/gmail/ncs/sapi/Main.java
+++ src/com/gmail/ncs/sapi/Main.java
@@ -1,12 +1,22 @@
 package com.gmail.ncs.sapi;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// call welcomeMessage from SimpleCalculator Class
-    SimpleCalculator.welcomeMessage();
-    // make an addition
-    SimpleCalculator first = new SimpleCalculator(4, 7);
+        
+        // call welcomeMessage from SimpleCalculator Class
+        SimpleCalculator.welcomeMessage();
+        System.out.println("Please enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        // input first number
+        double num1 = sc.nextDouble();
+        // input second number
+        System.out.println("Please enter a second number: ");
+        double num2 = sc.nextDouble();
+        // make an addition
+        SimpleCalculator first = new SimpleCalculator(num1, num2);
         System.out.println(first.number1 + first.number2);
     }
 }
--- src/com/gmail/ncs/sapi/SimpleCalculator.java
+++ src/com/gmail/ncs/sapi/SimpleCalculator.java
@@ -2,15 +2,15 @@ package com.gmail.ncs.sapi;
 
 public class SimpleCalculator {
 
-    int number1;
-    int number2;
+    double number1;
+    double number2;
 
     // this is a welcome message to practice how to create / use static method
     static void welcomeMessage(){
         System.out.println("Hello this is a simple calculator. Have FUN!");
     }
 
-    public SimpleCalculator(int a, int b){
+    public SimpleCalculator(double a, double b){
         number1 = a;
         number2 = b;
     }
