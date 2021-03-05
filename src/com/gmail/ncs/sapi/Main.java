5f8fe07cb142eec945415b68070db7ebc8bad287
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-05 00:01:42 +0000

    add message method. This is a static method to welcome the user. Purpose: refresh how to use static methods.)

--- src/com/gmail/ncs/sapi/Main.java
+++ src/com/gmail/ncs/sapi/Main.java
@@ -3,7 +3,9 @@ package com.gmail.ncs.sapi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// call welcomeMessage from SimpleCalculator Class
+    SimpleCalculator.welcomeMessage();
+    // make an addition
     SimpleCalculator first = new SimpleCalculator(4, 7);
         System.out.println(first.number1 + first.number2);
     }
--- src/com/gmail/ncs/sapi/SimpleCalculator.java
+++ src/com/gmail/ncs/sapi/SimpleCalculator.java
@@ -5,6 +5,11 @@ public class SimpleCalculator {
     int number1;
     int number2;
 
+    // this is a welcome message to practice how to create / use static method
+    static void welcomeMessage(){
+        System.out.println("Hello this is a simple calculator. Have FUN!");
+    }
+
     public SimpleCalculator(int a, int b){
         number1 = a;
         number2 = b;
